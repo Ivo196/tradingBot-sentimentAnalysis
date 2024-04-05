947e165129cc43005130012a5b51e97a6328ccfe
Ivo196 <ivo.tonioni@gmail.com>
2024-04-06 00:45:39 +0200

    feat

--- tradingBot.py
+++ tradingBot.py
@@ -2,8 +2,9 @@ from lumibot.brokers import Alpaca
 from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
-from datetime import datetime, timedelta
-from alpaca_trade_api import  REST
+from datetime import datetime 
+from alpaca_trade_api import REST 
+from timedelta import Timedelta 
 from finbert_utils import estimate_sentiment
 
 API_KEY = "PKNZDTSA16BY5RHV2L8S"
@@ -17,74 +18,80 @@ ALPACA_CREDS = {
 }
 
 
-class MLTrade(Strategy):
-    def initialize(self, symbol:str="SPY", cash_at_risk:float=0.5):
-        self.symbol = symbol #Hacemos que este como atributo de nuestra clase 
-        self.sleeptime = "24H"
-        self.last_trade = None #Por si queremos deshacer algunas de nuestras compra/venta
-        self.cash_at_risk = float(cash_at_risk)
+class MLTrader(Strategy): 
+    def initialize(self, symbol:str="SPY", cash_at_risk:float=.5): 
+        self.symbol = symbol
+        self.sleeptime = "24H" 
+        self.last_trade = None 
+        self.cash_at_risk = cash_at_risk
         self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
-        
-    def get_dates(self):  #Necesitamos la fecha de cuando estamos operando, para obtener la noticia de ese dia 
-        today = datetime.now()
-        three_days_prior = today - timedelta(days=3)
-        return today.strftime("%Y-%m-%d"), three_days_prior.strftime("%Y-%m-%d")
-    
-    def get_sentiment(self):
+
+    def position_sizing(self): 
+        cash = self.get_cash() 
+        last_price = self.get_last_price(self.symbol)
+        quantity = round(cash * self.cash_at_risk / last_price,0)
+        return cash, last_price, quantity
+
+    def get_dates(self): 
+        today = self.get_datetime()
+        three_days_prior = today - Timedelta(days=3)
+        return today.strftime('%Y-%m-%d'), three_days_prior.strftime('%Y-%m-%d')
+
+    def get_sentiment(self): 
         today, three_days_prior = self.get_dates()
-        news = self.api.get_news(symbol=self.symbol, start=three_days_prior, end=today)
-        
+        news = self.api.get_news(symbol=self.symbol, 
+                                 start=three_days_prior, 
+                                 end=today) 
         news = [ev.__dict__["_raw"]["headline"] for ev in news]
         probability, sentiment = estimate_sentiment(news)
+        print(news)
+        print(probability, sentiment)
         return probability, sentiment 
-        
-    def position_sizing(self):
-        cash = self.get_cash()
-        last_price = self.get_last_price(self.symbol)
-        quantity = round(cash * self.cash_at_risk / last_price, 0)
-        return cash, last_price, quantity
-        
+
     def on_trading_iteration(self):
         cash, last_price, quantity = self.position_sizing() 
         probability, sentiment = self.get_sentiment()
-        print(probability, sentiment)
-        if cash > last_price:
-            if sentiment == "positive" and probability > 0.93: 
+        
+
+        if cash > last_price: 
+            if sentiment == "positive" and probability > .98: 
                 if self.last_trade == "sell": 
                     self.sell_all() 
                 order = self.create_order(
                     self.symbol, 
                     quantity, 
                     "buy", 
-                    #type="bracket", 
+                     
                     take_profit_price=last_price*1.20, 
                     stop_loss_price=last_price*.95
                 )
                 self.submit_order(order) 
                 self.last_trade = "buy"
-            elif sentiment == "negative" and probability > 0.93: 
+            elif sentiment == "negative" and probability > .98: 
                 if self.last_trade == "buy": 
                     self.sell_all() 
                 order = self.create_order(
                     self.symbol, 
                     quantity, 
                     "sell", 
-                    #type="bracket", 
+                     
                     take_profit_price=last_price*.8, 
                     stop_loss_price=last_price*1.05
                 )
                 self.submit_order(order) 
                 self.last_trade = "sell"
-    
-start_date = datetime(2022,1,1)
-end_date =  datetime(2023,12,31)
-broker = Alpaca(ALPACA_CREDS)
-strategy = MLTrade(name='mlstrat', broker=broker, parameters={"symbol":"SPY", "cash_at_risk":"0.5"})
+
+start_date = datetime(2020,12,1)
+end_date = datetime(2023,12,31) 
+broker = Alpaca(ALPACA_CREDS) 
+strategy = MLTrader(name='mlstrat', broker=broker, 
+                    parameters={"symbol":"SPY", 
+                                "cash_at_risk":.5})
 strategy.backtest(
-    YahooDataBacktesting,
-    start_date,
-    end_date,
-    parameters={"symbol":"SPY", "cash_at_risk":"0.5"}  #Si queremos un bot con mas riesgo, configuramos cash_at_risk
+    YahooDataBacktesting, 
+    start_date, 
+    end_date, 
+    parameters={"symbol":"SPY", "cash_at_risk":.5}
 )
 # trader = Trader()
 # trader.add_strategy(strategy)
